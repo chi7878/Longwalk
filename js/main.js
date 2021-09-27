55b3ae4394862d5aacba694327961eee08c8d296
YuChi <percy860407@gmail.com>
2021-09-27 20:23:09 +0800

    edit

--- js/main.js
+++ js/main.js
@@ -52,10 +52,10 @@ $(document).ready(function () {
 
                 if (id === '17') {
                     let strHtml = '';
-                    const list = [{title: '開發旅行社 - 富陽三寶變變變 + 101景觀台', href: 'https://www.kaifa04376942.com.tw/act0710.html'}, {title: '丘山行 - 富陽三寶變變變 + 一日郊山健行挑戰', href: 'https://hillmont.tw/activities/greenyourjourney/'}, {title: '易遊網 - 富陽三寶變變變 + 台北探索館', href: 'https://trip.eztravel.com.tw/domestic/introduction/ODT0000009355'}, {title: '永恆旅行社 - 富陽三寶變變變 + 101景觀台', href: 'https://www.kaifa04376942.com.tw/E-act0710.html'}, {title: 'Accupass活動通 - 富陽三寶變變變', href: 'https://www.accupass.com/event/2109220659461191038393'}];
+                    const list = [{title: '開發旅行社 - 富陽三寶變變變 + 101景觀台', href: 'https://www.kaifa04376942.com.tw/act0710.html'}, {title: '丘山行 - 富陽三寶變變變 + 一日郊山健行挑戰', href: 'https://hillmont.tw/activities/greenyourjourney/'}, {title: '易遊網 - 富陽三寶變變變 + 台北探索館', href: 'https://trip.eztravel.com.tw/domestic/introduction/ODT0000009355'}, {title: '永恆旅行社 - 富陽三寶變變變 + 101景觀台', href: 'https://www.kaifa04376942.com.tw/E-act0710.html'}];
 
-                    list.forEach(function(data) {
-                        strHtml += `<div><p>${data.title}</p><a href="${data.href}" target="_blank"><img src="images/專屬遊程+報名按鈕圖檔1.png"></a></div>`;
+                    list.forEach(function(data, i) {
+                        strHtml += `<div><p>${data.title}</p><a ${i == 1 || i == 2 ? '' : `href="${data.href}"`} target="_blank"><img src="images/專屬遊程+報名按鈕圖檔${i == 1 || i == 2 ? '2' : '1'}.png"></a></div>`;
                     })
 
                     $('.theme-info > .theme-btn').hide();
